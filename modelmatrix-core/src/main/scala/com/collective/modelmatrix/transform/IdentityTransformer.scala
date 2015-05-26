408f4978fe57f8571c17ceaf0606d0e07fdcdeb4
Eugene Zhulenev <ezhulenev@collective.com>
2015-05-26 13:25:51 -0400

    GC-370 : add identity transformer

---
+++ modelmatrix-core/src/main/scala/com/collective/modelmatrix/transform/IdentityTransformer.scala
@@ -0,0 +1,27 @@
+package com.collective.modelmatrix.transform
+
+import com.collective.modelmatrix.ModelFeature
+import com.collective.modelmatrix.transform.InputSchemaError.{ExtractColumnNotFound, UnsupportedTransformDataType}
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types._
+
+import scalaz.\/
+import scalaz.syntax.either._
+
+class IdentityTransformer(input: DataFrame) extends Transformer(input) {
+
+  private val supportedDataTypes = Seq(ShortType, IntegerType, LongType, DoubleType)
+
+  def validate: PartialFunction[ModelFeature, \/[InputSchemaError, TypedModelFeature]] = {
+    case f@ModelFeature(_, _, _, e, Identity) if inputDataType(e).isEmpty =>
+      ExtractColumnNotFound(e).left
+
+    case f@ModelFeature(_, _, _, e, Identity)
+      if inputDataType(e).isDefined && supportedDataTypes.contains(inputDataType(e).get) =>
+      TypedModelFeature(f, inputDataType(e).get).right
+
+    case f@ModelFeature(_, _, _, e, t@Identity) =>
+      UnsupportedTransformDataType(e, inputDataType(e).get, t).left
+  }
+
+}
--- modelmatrix-core/src/main/scala/com/collective/modelmatrix/transform/package.scala
+++
@@ -1,8 +0,0 @@
-package com.collective.modelmatrix
-
-/**
- * Created by eugenezhulenev on 5/19/15.
- */
-package object transform {
-
-}
